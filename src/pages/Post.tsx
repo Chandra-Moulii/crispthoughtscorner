ad30d9bf25ab013e51ceeeca5dd51a1192067851
Chandra-Moulii <chandramouliv22@gmail.com>
2024-02-21 14:42:19 +0530

    style: fix fullscreen svg color to dyamic

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -244,7 +244,7 @@ export default function Post() {
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 -960 960 960"
-                    className="aspect-square w-6"
+                    className="aspect-square w-6 fill-skin-color"
                   >
                     <path d="M240-120v-120H120v-80h200v200h-80Zm400 0v-200h200v80H720v120h-80ZM120-640v-80h120v-120h80v200H120Zm520 0v-200h80v120h120v80H640Z" />
                   </svg>
@@ -252,7 +252,7 @@ export default function Post() {
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 -960 960 960"
-                    className="aspect-square w-5"
+                    className="aspect-square w-5 fill-skin-color"
                   >
                     <path d="M120-120v-200h80v120h120v80H120Zm520 0v-80h120v-120h80v200H640ZM120-640v-200h200v80H200v120h-80Zm640 0v-120H640v-80h200v200h-80Z" />
                   </svg>
