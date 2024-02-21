018833e3712b2d70ae60ca2209e361aa19329011
Chandra-Moulii <chandramouliv22@gmail.com>
2024-02-21 14:45:06 +0530

    style: change fullsceen svg size

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -244,7 +244,7 @@ export default function Post() {
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 -960 960 960"
-                    className="aspect-square w-6 fill-skin-color"
+                    className="aspect-square w-5 fill-skin-color"
                   >
                     <path d="M240-120v-120H120v-80h200v200h-80Zm400 0v-200h200v80H720v120h-80ZM120-640v-80h120v-120h80v200H120Zm520 0v-200h80v120h120v80H640Z" />
                   </svg>
